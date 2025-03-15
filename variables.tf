38747e78ddab26184849f24c437346e1c886cf6e
August Alexander <aalexand@rednaxela.technology>
2025-03-15 14:10:48 -0400

    added variables.tf

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "vpc-Name" {
+  type = string
+  default = "poolicio.us-vpc"
+}
+
+data "aws_vpc" "the-vpc" {
+  filter {
+    name = "tag:Name"
+    values = [var.vpc-Name]
+  }
+}
+
+data "aws_subnets" "the-subnets" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.the-vpc.id]
+  }
+}
+
+data "aws_subnet" "the-subnet" {
+  for_each = toset(data.aws_subnets.the-subnets.ids)
+  id = each.value
+}
+
+# Get latest Amazon Linux AMI
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.6.*.*-kernel-6.1-x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
