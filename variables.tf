a071d9c585e05e2d1adb889d8bc8c4f33fb5fd9b
August Alexander <aalexand@rednaxela.technology>
2025-03-15 14:52:13 -0400

    remove error

--- variables.tf
+++ variables.tf
@@ -36,12 +36,3 @@ data "aws_ami" "amazon-linux" {
   }
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  sensitive   = true
-}
