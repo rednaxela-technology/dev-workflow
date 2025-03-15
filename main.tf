7c9bb26d631758a3a3c22003abe2dcc6ae86bf55
August Alexander <aalexand@rednaxela.technology>
2025-03-15 14:54:49 -0400

    one more test

--- main.tf
+++ main.tf
@@ -25,8 +25,6 @@ terraform {
 
     provider "aws" {
         region     = "us-east-1"
-        access_key = var.aws_access_key
-        secret_key = var.aws_secret_key
     }    
 
     # An example resource that does nothing.
