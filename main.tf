900a5d64fa93939d9c85e2a2a424541efc6627e8
August Alexander <aalexand@rednaxela.technology>
2025-03-16 10:34:07 -0400

    trying with env variables

--- main.tf
+++ main.tf
@@ -24,27 +24,27 @@ resource "null_resource" "example" {
   }
 }
 
-# resource "random_id" "index" {
-#     byte_length = 2
-# }
+resource "random_id" "index" {
+    byte_length = 2
+}
 
-# locals {
-#     subnets_list = tolist(data.aws_subnets.the-subnets.ids)
-#     subnets_random_index = random_id.index.dec % length(data.aws_subnets.the-subnets.ids)
-#     instance_subnet_id = local.subnets_list[local.subnets_random_index]
-# }
+locals {
+    subnets_list = tolist(data.aws_subnets.the-subnets.ids)
+    subnets_random_index = random_id.index.dec % length(data.aws_subnets.the-subnets.ids)
+    instance_subnet_id = local.subnets_list[local.subnets_random_index]
+}
 
-# resource "aws_instance" "linux_server" {
-#     ami = data.aws_ami.amazon-linux.id
-#     instance_type = "t2.micro"
-#     subnet_id = local.instance_subnet_id
-#     associate_public_ip_address = true
-#     key_name = "terraform-ec2"
-#     iam_instance_profile = "3HDAmazonSSMManagedInstanceCore"
+resource "aws_instance" "linux_server" {
+    ami = data.aws_ami.amazon-linux.id
+    instance_type = "t2.micro"
+    subnet_id = local.instance_subnet_id
+    associate_public_ip_address = true
+    key_name = "terraform-ec2"
+    iam_instance_profile = "3HDAmazonSSMManagedInstanceCore"
     
-#     tags = {
-#       Name = "Github-Actions-Test"  # yes This will be the name shown in AWS Console
-#     }
+    tags = {
+      Name = "Github-Actions-Test"  # yes This will be the name shown in AWS Console
+    }
 }
 
     
